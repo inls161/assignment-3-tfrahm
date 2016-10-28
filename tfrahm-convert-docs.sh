08c915e802314d5666a8e020f79b459c1f6b4f72
Tabitha Frahm <tabitha.frahm@gmail.com>
2016-10-28 01:05:54 +0000

    clarified comments

--- tfrahm-convert-docs.sh
+++ tfrahm-convert-docs.sh
@@ -6,6 +6,7 @@ echo -e "Hello. Which \e[31m.md\e[0m file would you like to convert?"
 # Declare input argument as a variable
 read OUTPUT
 
+# Remove file extension from OUTPUT (if need be).
 if [[ $OUTPUT == *".md" ]]; then  
     FILE=${OUTPUT%.*};
  else
@@ -39,12 +40,6 @@ pandoc -o $FILE.pdf $FILE.md
 # Make the command line read "Converted $FILENAME to HTML, DOCX, ODT, PDF
 echo -e "Converted \e[31m$FILE.md\e[0m to \e[36mHTML\e[0m, \e[36mDOCX\e[0m, \e[36mODT\e[0m, and \e[36mPDF\e[0m."
 
+# Be courteous!
 echo "Have a nice day!"
 
-
-
-# Notes to self:
-
-# add if statement? -if ANSWER.md else ANSWER
-# add function??
-# templates?
