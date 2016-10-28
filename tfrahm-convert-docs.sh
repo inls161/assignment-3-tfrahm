acd1efae7f9278a2392740cf53f91cc66f5aa8ec
Tabitha Frahm <tabitha.frahm@gmail.com>
2016-10-28 00:27:27 +0000

    fancified my script

--- tfrahm-convert-docs.sh
+++ tfrahm-convert-docs.sh
@@ -1,37 +1,43 @@
 #!/bin/bash
 
-# Ask for INPUT-FILENAME
+# Ask for OUTPUT
 echo -e "Hello. Which \e[31m.md\e[0m file would you like to convert?"
 
 # Declare input argument as a variable
-read FILENAME
+read OUTPUT
 
+if [[ $OUTPUT == *".md" ]]; then  
+    FILE=${OUTPUT%.*};
+ else
+    FILE=$OUTPUT;
+fi
+  
 # Say "Converting to HTML..."
 echo -e "Converting to \e[36mHTML\e[0m..."
 
 # Convert markdown to HTML
-pandoc -o $FILENAME.html $FILENAME.md
+pandoc -o $FILE.html $FILE.md
 
 # Say "Converting to DOCX..."
 echo -e "Converting to \e[36mDOCX\e[0m..."
 
 # Convert markdown to DOCX
-pandoc -o $FILENAME.docx $FILENAME.md
+pandoc -o $FILE.docx $FILE.md
 
 # Say "Converting to ODT..."
 echo -e "Converting to \e[36mODT\e[0m..."
 
 # Convert markdown to ODT
-pandoc -o $FILENAME.odt $FILENAME.md
+pandoc -o $FILE.odt $FILE.md
 
 #Say "Converting to PDF..."
 echo -e "Converting to \e[36mPDF\e[0m..."
 
 # Convert markdown to PDF
-pandoc -o $FILENAME.pdf $FILENAME.md
+pandoc -o $FILE.pdf $FILE.md
 
 # Make the command line read "Converted $FILENAME to HTML, DOCX, ODT, PDF
-echo -e "Converted \e[31m$FILENAME.md\e[0m to \e[36mHTML\e[0m, \e[36mDOCX\e[0m, \e[36mODT\e[0m, and \e[36mPDF\e[0m."
+echo -e "Converted \e[31m$FILE.md\e[0m to \e[36mHTML\e[0m, \e[36mDOCX\e[0m, \e[36mODT\e[0m, and \e[36mPDF\e[0m."
 
 echo "Have a nice day!"
 
@@ -42,4 +48,3 @@ echo "Have a nice day!"
 # add if statement? -if ANSWER.md else ANSWER
 # add function??
 # templates?
-# smartquotes?
